7fb5ee4205d5b5bac56cd506173ffd79ba8cb497
Ehetashamul Haque <hehetashamul@gmail.com>
2026-07-11 21:33:13 +0530

    add rg

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,14 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example"
+resource "azurerm_resource_group" "rg1" {
+  name     = "prod1"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "prod2"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "prod2"
   location = "West Europe"
 }
